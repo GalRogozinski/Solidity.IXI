68f79f2ffcda9106fbc728b8fd188eacf5b8790a
galrogo <gal.rogozinski@iota.org>
2020-03-08 19:40:05 +0200

    return

--- Solidity.ixi/index.js
+++ Solidity.ixi/index.js
@@ -1,7 +1,7 @@
 var transactionValidator = IOTA.transactionValidator
 
 function checkSolidity(hash) {
-    transactionValidator.checkSolidity(com.iota.iri.model.HashFactory.Transaction.create(hash))
+    return transactionValidator.checkSolidity(com.iota.iri.model.HashFactory.Transaction.create(hash))
 }
 
 API.put("checkSolidity", new Callable({ call: checkSolidity }))
